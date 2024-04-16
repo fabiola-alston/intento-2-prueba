2263255802c6d6378172626a9c9fe717731232e3
TamaraCM06 <167063588+TamaraCM06@users.noreply.github.com>
2024-04-16 08:41:02 -0600

    nocreo

---
+++ killme.py
@@ -0,0 +1,12 @@
+def minmax(lista):
+    if isinstance(lista, list) and len(lista)>2:
+        return minmax_aux(lista, 0, 0, 0, [])
+    else:
+        return "Error"
+def minmax_aux(lista, n,  minimo, maximo, new):
+    if lista == []:
+        return new
+    elif lista[n] > maximo:
+        maximo = lista[n]
+        new
+        return lista, n+1, minimo, maximo, new
